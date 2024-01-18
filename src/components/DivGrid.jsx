8c5f0c3590d2f8f17e3015da8303064baaddafe1
Joseph Martinez <joseph.r.martinez@gmail.com>
2024-01-18 13:44:53 -0700

    Implement generate new terms feature

--- src/components/DivGrid.jsx
+++ src/components/DivGrid.jsx
@@ -1,9 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { needsList } from '../assets/needsList';
+import { initialFeelingsList } from '../assets/initialFeelingsList';
+import { underlyingFeelingsList } from '../assets/underlyingFeelingsList';
 
 export default function DivGrid({ words, section, bgColor, updateFields, handleDivClick}) {
   const [selectedIndexes, setSelectedIndexes] = useState([]);
   const [userInputActive, setUserInputActive] = useState(false)
   const [userInputValue, setUserInputValue] = useState("")
+  const [refreshActive, setRefreshActive] = useState(false)
+  const [wordArray, setWordArray] = useState([])
+
+
+  useEffect(() =>{
+  // Select word list based on current section:
+  if (section === "underlyingFeelings"){
+    setWordArray(underlyingFeelingsList)
+  } else if (section === "initialFeelings") {
+    setWordArray(initialFeelingsList) 
+  } else if (section === "needs") {
+    setWordArray(needsList)
+  }
+  }, [section])
+  
 
   function handleAddClick() {
     setUserInputActive(!userInputActive)
@@ -31,12 +49,94 @@ export default function DivGrid({ words, section, bgColor, updateFields, handleD
     }
   }
 
+  function handleRefreshClick(){
+    setRefreshActive(true)
+
+    // Count number of words currently unselected
+    // Return array of currently unselected words
+    
+    const selectedFalse = returnSelectedFalse(words)
+    console.log("selectedFalse:", selectedFalse)
+
+    console.log("words:", words)
+
+    // Extract number of random words based on number not yet selected
+    let options = getNewOptions(wordArray, words, selectedFalse)
+    console.log("new options:", options)
+
+    // Update words array with new values
+    const newWords = replaceWords(words, options)
+    // console.log("newWords:", newWords)
+
+    setTimeout(() => {
+      setRefreshActive(false)
+    }, 1000);
+  }
+
+
+  function returnSelectedFalse(objectsArray) {
+    let selectedFalse = []
+
+    for (const obj of objectsArray) {
+      if (obj.selected === false) {
+        selectedFalse.push(obj.word)
+      }
+    }
+
+    return selectedFalse
+  }
+
+  function getRandomItems(array, count) {
+    const newArray = array.slice();
+  
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+  
+    return newArray.slice(0, count);
+  }
+
+  function getNewOptions(wordList, currentOptions, selectedFalse) {
+    // Create a Set of words to remove
+    const wordsToRemove = new Set(currentOptions.map(option => option.word));
+  
+    // Filter the wordList to remove words in the Set
+    const newArray = wordList.filter(word => !wordsToRemove.has(word));
+  
+    // Return selectedFalse.length number of values from newArray. Select these at random.
+    const randomOptions = getRandomItems(newArray, selectedFalse.length);
+  
+    return randomOptions;
+  }
+
+  function replaceWords(currentWords, newOptions) {
+    let choiceIndex = 0;
+  
+    for (let i = 0; i < currentWords.length; i++) {
+      if (!currentWords[i].selected) {
+        currentWords[i].word = newOptions[choiceIndex];
+        choiceIndex++;
+  
+        // Reset index when all choices are used
+        if (choiceIndex === newOptions.length) {
+          choiceIndex = 0;
+        }
+      }
+    }
+  
+    return currentWords;
+  }
+
+//  Go through the array of words. For each value that has selected === false, replace the value with a random value from the same section of words. 
+
+
   return (
     <div className="grid grid-cols-2 gap-6">
       {words.map((word, index) => (
         <div
           key={index}
-          className={`py-2 px-3 rounded-full cursor-pointer select-none transition-colors duration-200 antialiased`}
+          className={`py-2 px-3 rounded-full cursor-pointer select-none transition-colors duration-300 antialiased`}
           style={word.selected
             ? { color: '#FEFEFE', backgroundColor: bgColor, borderWidth: '1px', borderColor: bgColor }
             : { color: '#606060', borderWidth: '1px', borderColor: '#c5c5c5'}}
@@ -58,6 +158,10 @@ export default function DivGrid({ words, section, bgColor, updateFields, handleD
             onKeyDown={handleKeyDown}
           ></input></div>
         : <div onClick={handleAddClick} className="cursor-pointer text-3xl select-none" style={{ color: '#545454'}}>+</div>}
+        <div onClick={handleRefreshClick} className='flex flex-row items-center justify-center h-10 cursor-pointer'>
+          <img src='/arrows-clockwise.png' className={`w-7 ${refreshActive && `animate-spin`}`}></img>
+        </div>
     </div>
+    
   );
 }
